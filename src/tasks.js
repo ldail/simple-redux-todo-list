d1fef5d8279b55b3907300ec0a211ffb2159ca84
ldail <leondailofficial@gmail.com>
2020-01-21 12:20:29 -0800

    converted all ids to UUIDv4

--- src/tasks.js
+++ src/tasks.js
@@ -1,8 +1,10 @@
+import {uuid} from 'uuidv4'
+
 let tasks = [
-  {id: 1, task: 'Abigail needs clothes', completed: false},
-  {id: 2, task: 'Bring the clothes', completed: false},
-  {id: 3, task: 'Clean the rooms', completed: false},
-  {id: 4, task: 'Do the dishes', completed: false}
+  {id: uuid(), task: 'Abigail needs clothes', completed: false},
+  {id: uuid(), task: 'Bring the clothes', completed: false},
+  {id: uuid(), task: 'Clean the rooms', completed: false},
+  {id: uuid(), task: 'Do the dishes', completed: false}
 ]
 
 export default tasks;
